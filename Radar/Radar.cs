b9f4117b4cc607c9c7315def1efa864b5a467bcb
leonana69 <guojun.chen@yale.edu>
2024-04-02 10:08:27 -0400

    Update version to 1.1.2

--- Radar/Radar.cs
+++ Radar/Radar.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace Radar
 {
-    [BepInPlugin("Tyrian.Radar", "Radar", "1.1.1")]
+    [BepInPlugin("Tyrian.Radar", "Radar", "1.1.2")]
     public class Radar : BaseUnityPlugin
     {
         internal static Radar Instance {get; private set;}
